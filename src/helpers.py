cc62662e15a5be0357c65ef6d9b24c1a964bd950
Raiane Oliveira <raiane.oliveira404@gmail.com>
2024-11-14 14:06:31 -0300

    add helpers file

---
+++ src/helpers.py
@@ -0,0 +1,9 @@
+def goBackToMenu():
+    # TODO: (kill) método que pergunta ao usuário se ele quer voltar ao menu ou encerrar o programa
+    return
+
+
+def getPositiveInteger():
+    """Retorna um inteiro positivo válido e imprime 'Opção inválida' caso seja inválido"""
+    # TODO: (kill)
+    return
