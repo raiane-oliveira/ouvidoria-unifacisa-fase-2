5de3f1d9f5c503feeb7b8dbc90c03df76002549f
killandio <killandio@keymail.dev>
2024-11-18 15:44:06 -0300

    Primeira versão dos Métodos Auxiliares
    :wq

--- src/helpers.py
+++ src/helpers.py
@@ -1,9 +1,23 @@
+"""
 def goBackToMenu():
-    # TODO: (kill) método que pergunta ao usuário se ele quer voltar ao menu ou encerrar o programa
-    return
-
+    # Pergunta ao usuário se ele quer voltar ao menu ou encerrar o programa
 
 def getPositiveInteger():
-    """Retorna um inteiro positivo válido e imprime 'Opção inválida' caso seja inválido"""
-    # TODO: (kill)
-    return
+    # Retorna um inteiro positivo válido e imprime 'Opção inválida' caso seja inválido
+"""
+
+def goBackToMenu():
+    escolha = -1
+    while escolha != 2:
+        print("1) Voltar ao menu principal\n2) Encerrar o programa de Ouvidoria")
+        if escolha == 1:
+            print("Voltando ao menu principal")
+        elif escolha <=0 or escolha >= 3:
+            print("Opção inválida. Por favor, tente novamente.")
+
+def getPositiveInteger():
+    numero = -1
+    while numero < 0:
+        numero = int(input("Digite o número que corresponde a opção desejada"))
+        if numero <= 0 or numero > 7:
+            print("Número inválido. Por favor, tente novamente.")
