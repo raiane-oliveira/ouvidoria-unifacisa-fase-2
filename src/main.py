3b160304ee62148e3c025d464d489b8a6e3608da
Raiane Oliveira <raiane.oliveira404@gmail.com>
2024-11-22 14:55:07 -0300

    feat: call methods to menu

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from use_cases import *
+from helpers import goBackToMenu
 
 conexao = criarConexao("localhost", "root", "unifacisa", "ouvidoria")
 
 def main():
-    opcao = 0
+    opcao = ""
 
     while opcao != "7":
         print("Seja bem-vindo ao sistema de ouvidoria da Unifacisa!\n")
@@ -18,54 +19,45 @@ def main():
         opcao = input("Digite a sua opção: ")
 
         if opcao == "1":
-            print("\n1) Listagem das Manifestações\n")
-
-            # TODO: (fernanda) método para listagem
+            print("\n1) Listagem das Manifestações")
             listarManifestacoes(conexao)
 
             print("-" * 54)
-            # opcao = goBackToMenu()
+            opcao = goBackToMenu()
         elif opcao == "2":
             print("\n2) Listagem das Manifestações por Tipo\n")
-
-            # TODO: (duda) método para listagem por tipo
+            listandoManifestacoesPorTipo(conexao)
 
             print("-" * 54)
-            # opcao = goBackToMenu()
+            opcao = goBackToMenu()
         elif opcao == "3":
-            #print("\n3) Criar uma nova manifestação\n")
+            print("\n3) Criar uma nova manifestação")
             executarMenuCriar(conexao)
-
-            # TODO: (Alan) método para criação
 
             print("-" * 54)
         elif opcao == "4":
             print("\n4) Exibir quantidade de manifestações\n")
 
-            consultaManifesto = 'select count(*) from codigo'
+            consultaManifesto = 'select count(*) from manifestacoes'
             codigo = listarBancoDados(conexao, consultaManifesto)
             quantidade = codigo[0][0]
 
             print("A quantidade total de manifestações é", quantidade, "\n")
 
-            # TODO: (pedro) método para quantidade
-
             print("-" * 54)
-            # opcao = goBackToMenu()
+            opcao = goBackToMenu()
         elif opcao == "5":
             print("\n5) Pesquisar uma manifestação por código\n")
             pesquisarPorCodigo(conexao)
-            # TODO: (letícya) método para pesquisa
 
             print("-" * 54)
-            # opcao = goBackToMenu()
+            opcao = goBackToMenu()
         elif opcao == "6":
             print("\n6) Excluir uma Manifestação pelo Código\n")
-
-            # TODO: (kaio) método para exclusão
+            excluir_manifestacao(conexao)
 
             print("-" * 54)
-            # opcao = goBackToMenu()
+            opcao = goBackToMenu()
         elif opcao != "7":
             print("Opção inválida\n")
 
